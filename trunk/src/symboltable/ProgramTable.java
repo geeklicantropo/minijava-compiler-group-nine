978564572e517f79ef4e1d39ac06a397123afc0e
Pyuras <Pyuras@4eaeb328-73ce-13fc-2d4b-f47fba027791>
2012-04-09 00:43:24 +0000

    Tabela de Símbolos.

---
+++ trunk/src/symboltable/ProgramTable.java
@@ -0,0 +1,22 @@
+package symboltable;
+
+import java.util.Hashtable;
+
+public class ProgramTable {
+	
+	private final Hashtable<Symbol, ClassTable> programClasses = new Hashtable<Symbol, ClassTable>();
+    
+    public boolean addClass(String className, ClassTable value){
+            if (programClasses.containsKey(Symbol.symbol(className))) return false;
+            else{
+                    programClasses.put(Symbol.symbol(className),value);
+                    return true;
+            }
+    }
+    
+    public ClassTable getClass(Symbol className){
+            if(programClasses.containsKey(className)) return programClasses.get(className);
+            else return null;
+    }
+
+}

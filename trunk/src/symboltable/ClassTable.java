b67facb29102cdc20f2fa0aa68081604d90b04ec
vitoriopp@gmail.com <vitoriopp@gmail.com@4eaeb328-73ce-13fc-2d4b-f47fba027791>
2012-04-09 06:39:54 +0000

    Entrada do programa Sample4.txt, pois este não possui comentários dentro de seu código. Análise léxica funcionando, aguardando término das etapas para análise semântica.

--- trunk/src/symboltable/ClassTable.java
+++ trunk/src/symboltable/ClassTable.java
@@ -1,3 +1,4 @@
+
 package symboltable;
 
 import java.util.Hashtable;
@@ -6,29 +7,29 @@ import syntaxtree.Type;
 
 public class ClassTable {
 
-	private final Hashtable<Symbol, Type> classFields = new Hashtable<Symbol, Type>();
+        private final Hashtable<Symbol, Type> classFields = new Hashtable<Symbol, Type>();
 
-	private final Hashtable<Symbol, MethodTable> classMethods = new Hashtable<Symbol, MethodTable>();
+        private final Hashtable<Symbol, MethodTable> classMethods = new Hashtable<Symbol, MethodTable>();
 
-	public boolean addField(String field, Type value) {
-		if (classFields.containsKey(field))
-			return false;
-		classFields.put(Symbol.symbol(field), value);
-		return true;
-	}
+        public boolean addField(String field, Type value) {
+                if (classFields.containsKey(field))
+                        return false;
+                classFields.put(Symbol.symbol(field), value);
+                return true;
+        }
 
-	public boolean addMethod(String methodName, Type value) {
-		if (classMethods.containsKey(methodName))
-			return false;
-		classFields.put(Symbol.symbol(methodName), value);
-		return true;
-	}
+        public boolean addMethod(String methodName, Type value) {
+                if (classMethods.containsKey(methodName))
+                        return false;
+                classFields.put(Symbol.symbol(methodName), value);
+                return true;
+        }
 
-	public MethodTable getMethod(Symbol methodName) {
-		if (classMethods.containsKey(methodName))
-			return classMethods.get(methodName);
-		else
-			return null;
-	}
+        public MethodTable getMethod(Symbol methodName) {
+                if (classMethods.containsKey(methodName))
+                        return classMethods.get(methodName);
+                else
+                        return null;
+        }
 
 }
--- trunk/src/symboltable/ProgramTable.java
+++ trunk/src/symboltable/ProgramTable.java
@@ -4,22 +4,22 @@ import java.util.Hashtable;
 
 public class ProgramTable {
 
-	private final Hashtable<Symbol, ClassTable> programClasses = new Hashtable<Symbol, ClassTable>();
+        private final Hashtable<Symbol, ClassTable> programClasses = new Hashtable<Symbol, ClassTable>();
 
-	public boolean addClass(String className, ClassTable value) {
-		if (programClasses.containsKey(Symbol.symbol(className)))
-			return false;
-		else {
-			programClasses.put(Symbol.symbol(className), value);
-			return true;
-		}
-	}
+        public boolean addClass(String className, ClassTable value) {
+                if (programClasses.containsKey(Symbol.symbol(className)))
+                        return false;
+                else {
+                        programClasses.put(Symbol.symbol(className), value);
+                        return true;
+                }
+        }
 
-	public ClassTable getClass(Symbol className) {
-		if (programClasses.containsKey(className))
-			return programClasses.get(className);
-		else
-			return null;
-	}
+        public ClassTable getClass(Symbol className) {
+                if (programClasses.containsKey(className))
+                        return programClasses.get(className);
+                else
+                        return null;
+        }
 
 }

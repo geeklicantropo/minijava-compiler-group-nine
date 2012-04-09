6bb8e8df9f8234bc3de9b35b3ef07e09ed32edcb
Pyuras <Pyuras@4eaeb328-73ce-13fc-2d4b-f47fba027791>
2012-04-09 01:05:06 +0000

    Implementação parcial do Visitor que cria a Tabela de Símbolos.

--- trunk/src/symboltable/ProgramTable.java
+++ trunk/src/symboltable/ProgramTable.java
@@ -3,20 +3,23 @@ package symboltable;
 import java.util.Hashtable;
 
 public class ProgramTable {
-	
+
 	private final Hashtable<Symbol, ClassTable> programClasses = new Hashtable<Symbol, ClassTable>();
-    
-    public boolean addClass(String className, ClassTable value){
-            if (programClasses.containsKey(Symbol.symbol(className))) return false;
-            else{
-                    programClasses.put(Symbol.symbol(className),value);
-                    return true;
-            }
-    }
-    
-    public ClassTable getClass(Symbol className){
-            if(programClasses.containsKey(className)) return programClasses.get(className);
-            else return null;
-    }
+
+	public boolean addClass(String className, ClassTable value) {
+		if (programClasses.containsKey(Symbol.symbol(className)))
+			return false;
+		else {
+			programClasses.put(Symbol.symbol(className), value);
+			return true;
+		}
+	}
+
+	public ClassTable getClass(Symbol className) {
+		if (programClasses.containsKey(className))
+			return programClasses.get(className);
+		else
+			return null;
+	}
 
 }

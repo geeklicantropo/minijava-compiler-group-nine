83d4f23a5f1fee0809dd6c5edcbfd3863629a3ca
Pyuras <Pyuras@4eaeb328-73ce-13fc-2d4b-f47fba027791>
2012-06-20 14:45:01 +0000

    Correções na implementação do Visitor responsável pela criação da Tabela de Símbolos.

--- src/symboltable/MethodTable.java
+++ src/symboltable/MethodTable.java
@@ -5,23 +5,51 @@ import java.util.Hashtable;
 import syntaxtree.Type;
 
 public class MethodTable {
+	
+		private String nome;
+		private Type tipo;
+		
+		public MethodTable (String nome, Type tipo) {
+			this.setNome(nome);
+			this.setTipo(tipo);
+		}
 
-        private final Hashtable<Symbol, Type> params = new Hashtable<Symbol, Type>();
+        public MethodTable() {
+			// TODO Auto-generated constructor stub
+		}
+
+		private final Hashtable<Symbol, Type> params = new Hashtable<Symbol, Type>();
 
         private final Hashtable<Symbol, Type> locals = new Hashtable<Symbol, Type>();
 
-        public boolean addParam(String param, Type value) {
+        public boolean addParam(Symbol param, Type value) {
                 if (params.containsKey(param))
                         return false;
-                params.put(Symbol.symbol(param), value);
+                params.put(param, value);
                 return true;
         }
 
-        public boolean addLocal(String local, Type value) {
+        public boolean addLocal(Symbol local, Type value) {
                 if (locals.containsKey(local))
                         return false;
-                locals.put(Symbol.symbol(local), value);
+                locals.put(local, value);
                 return true;
         }
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public Type getTipo() {
+			return tipo;
+		}
+
+		public void setTipo(Type tipo) {
+			this.tipo = tipo;
+		}
 
 }

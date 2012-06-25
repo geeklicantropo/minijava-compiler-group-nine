1de3c01c101e7861cd5d8d37066c1ff1886c2125
patrickhugo@gmail.com <patrickhugo@gmail.com@4eaeb328-73ce-13fc-2d4b-f47fba027791>
2012-06-25 22:05:41 +0000

    Frame

---
+++ trunk/src/Frame/Frame.java
@@ -0,0 +1,19 @@
+package Frame;
+
+import temp.*;
+
+public abstract class Frame implements TempMap
+{
+
+  public Label name;
+  public AccessList formals;
+  public abstract Frame newFrame(String name, int argCount);
+  public abstract int wordSize();
+  public abstract Access allocLocal(boolean escape);
+  public abstract Temp FP();
+  public abstract Temp RV();
+  public abstract Temp RA();
+  public abstract IRTree2.Exp externalCall(String func, IRTree2.ExpList args);
+  public abstract String tempMap(Temp temp);
+  public abstract Assem.InstrList codegen(IRTree2.Stm s);
+}
